89224fa7b0ec134cd1e7b80bb8ae00bcde14d66b
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-02-25 13:52:54 -0800

    Added an http client to interact with the API

--- app/src/main/java/com/github/ddenaga/flix/MainActivity.kt
+++ app/src/main/java/com/github/ddenaga/flix/MainActivity.kt
@@ -2,10 +2,33 @@ package com.github.ddenaga.flix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+// URL and API key to retrieve JSON data of movies "now playing" from the Movie DB.
+private const val API_KEY = BuildConfig.API_KEY
+private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=$API_KEY"
+
+// Logcat tag.
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Asynchronous HTTP client for making requests to the API.
+        val client = AsyncHttpClient()
+        client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler() {
+            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?) {
+                Log.e(TAG, "onFailure: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                Log.i(TAG, "onSuccess: JSON data $json")
+            }
+        })
     }
 }
