10c5f02b4c818a18b946ac0eaab4e37044bd4ece
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-02-26 18:24:21 -0800

    Added a YouTube video player to the detail activity

--- app/src/main/java/com/github/ddenaga/flix/activities/DetailActivity.kt
+++ app/src/main/java/com/github/ddenaga/flix/activities/DetailActivity.kt
@@ -1,21 +1,30 @@
 package com.github.ddenaga.flix.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.github.ddenaga.flix.BuildConfig
 import com.github.ddenaga.flix.R
 import com.github.ddenaga.flix.adapters.MOVIE_EXTRA
 import com.github.ddenaga.flix.models.Movie
+import com.github.ddenaga.flix.network.MovieClient
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
 
+private const val YT_API_KEY = BuildConfig.YT_API_KEY
 private const val TAG = "DetailActivity"
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : YouTubeBaseActivity() {
 
     private lateinit var tvTitle: TextView
     private lateinit var tvOverview: TextView
     private lateinit var tvPopularity: TextView
     private lateinit var rbVoteAverage: RatingBar
+    private lateinit var pvPlayer: YouTubePlayerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +34,7 @@ class DetailActivity : AppCompatActivity() {
         tvOverview = findViewById(R.id.tvOverview)
         tvPopularity = findViewById(R.id.tvPopularity)
         rbVoteAverage = findViewById(R.id.rbVoteAverage)
+        pvPlayer = findViewById(R.id.pvPlayer)
 
         val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
         Log.i(TAG, "Movie is $movie")
@@ -33,5 +43,38 @@ class DetailActivity : AppCompatActivity() {
         tvOverview.text = movie.overview
         tvPopularity.text = getString(R.string.popularity, movie.popularity)
         rbVoteAverage.rating = movie.voteAverage.toFloat()
+
+        val movieClient = MovieClient()
+        movieClient.getVideos(movie.movieId, object: JsonHttpResponseHandler() {
+            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?) {
+                Log.i(TAG, "onFailure: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess: $json")
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0) {
+                    Log.w(TAG, "No movie trailers found")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+
+                initializeYoutube(youtubeKey)
+            }
+        })
+    }
+
+    private fun initializeYoutube(youtubeKey: String) {
+        pvPlayer.initialize(YT_API_KEY, object: YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, p2: Boolean) {
+                Log.i(TAG, "onInitializationSuccess")
+                player?.cueVideo(youtubeKey) // "a0YrCABCOEY"
+            }
+
+            override fun onInitializationFailure(provider: YouTubePlayer.Provider?, result: YouTubeInitializationResult?) {
+                Log.i(TAG, "onInitializationFailure: $result")
+            }
+        })
     }
 }
