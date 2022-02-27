429ff056318eb267ae21e6af0591bb36150037df
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-02-26 16:48:38 -0800

    Implemented a click listener for view holders

--- app/src/main/java/com/github/ddenaga/flix/adapters/MovieAdapter.kt
+++ app/src/main/java/com/github/ddenaga/flix/adapters/MovieAdapter.kt
@@ -1,16 +1,21 @@
 package com.github.ddenaga.flix.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.github.ddenaga.flix.models.Movie
 import com.github.ddenaga.flix.R
+import com.github.ddenaga.flix.activities.DetailActivity
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,10 +30,14 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
@@ -37,6 +46,16 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 .load(movie.posterImageUrl).
                 placeholder(R.drawable.poster).
                 into(ivPoster)
+        }
+
+        override fun onClick(p0: View?) {
+            // Get the movie that was clicked
+            val movie = movies[adapterPosition]
+
+            // Setup an intent and navigate to the detail activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
