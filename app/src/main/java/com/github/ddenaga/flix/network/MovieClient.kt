d8b5bd26a98f0b637cbc0ab57c1802fc23571750
ddenaga <76412992+ddenaga@users.noreply.github.com>
2022-02-26 18:23:50 -0800

    Added a method to get videos from a movie ID

--- app/src/main/java/com/github/ddenaga/flix/network/MovieClient.kt
+++ app/src/main/java/com/github/ddenaga/flix/network/MovieClient.kt
@@ -9,12 +9,17 @@ class MovieClient {
 
     companion object {
         // URLs and API key to retrieve JSON data from the Movie DB.
-        private const val API_KEY = BuildConfig.API_KEY
+        private const val API_KEY = BuildConfig.TMDB_API_KEY
         private const val API_BASE_URL = "https://api.themoviedb.org/3"
         private const val NOW_PLAYING_URL = "$API_BASE_URL/movie/now_playing?api_key=$API_KEY"
+        private const val TRAILER_URL = "$API_BASE_URL/movie/%d/videos?api_key=$API_KEY"
     }
 
     fun getNowPlaying(handler: JsonHttpResponseHandler) {
         client.get(NOW_PLAYING_URL, handler)
+    }
+
+    fun getVideos(movieId: Int, handler: JsonHttpResponseHandler) {
+        client.get(TRAILER_URL.format(movieId), handler)
     }
 }
